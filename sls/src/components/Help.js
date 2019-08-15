d2032643026eece26eeb9fdef5dc54034433379b
seanchok <seanchok@gmail.com>
2019-08-16 03:07:23 +0800

    adjust css properties

--- sls/src/components/Help.js
+++ sls/src/components/Help.js
@@ -35,6 +35,14 @@ class Help extends React.Component {
              enim eiusmod high life accusamus terry richardson ad squid. Nihil
              anim keffiyeh helvetica, craft beer labore wes anderson cred
              nesciunt sapiente ea proident.
+             Anim pariatur cliche reprehenderit,
+              enim eiusmod high life accusamus terry richardson ad squid. Nihil
+              anim keffiyeh helvetica, craft beer labore wes anderson cred
+              nesciunt sapiente ea proident.
+              Anim pariatur cliche reprehenderit,
+               enim eiusmod high life accusamus terry richardson ad squid. Nihil
+               anim keffiyeh helvetica, craft beer labore wes anderson cred
+               nesciunt sapiente ea proident.
             </CardBody>
           </Card>
         </Collapse>
