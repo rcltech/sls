17f68f52357030dbab77ab54123e9610ac8e7062
seanchok <seanchok@gmail.com>
2019-08-19 15:28:46 +0800

    change contact email to contact@rctech.club

--- sls/src/components/Contact.js
+++ sls/src/components/Contact.js
@@ -4,12 +4,11 @@ import {Collapse, Card, CardBody } from 'reactstrap';
 
 class Contact extends React.Component {
   render() {
-      const {collapse, collapseStyles, textBodyStyles} = this.props;
     return (
-        <Collapse isOpen={collapse} style={collapseStyles}>
+        <Collapse isOpen={this.props.collapse} style={this.props.collapseStyles}>
           <Card>
-            <CardBody style={textBodyStyles}>
-            To report any problems with SLS, please <a href="mailto:rcltechclub@gmail.com?subject=SLS%20Problem">contact us by email</a>.
+            <CardBody style={this.props.textBodyStyles}>
+            To report any problems with SLS, please <a href="mailto:contact@rctech.club?subject=SLS%20Problem">contact us by email</a>.
             </CardBody>
           </Card>
         </Collapse>
