e30f521f20f376695ad2d77d17fd23a226d8242b
seanchok <seanchok@gmail.com>
2019-09-05 00:10:14 +0800

    setup body-parser to parse req.body data
    #22

--- ec2/index.js
+++ ec2/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const PORT = 4000;
 
 app.post('/', (req, res, next) => {
+  console.log(req.body);
   res.send("post request received");
 })
 
