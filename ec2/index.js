fb4fdaab76532c98cebff2fea210e0b415d602f9
seanchok <seanchok@gmail.com>
2019-09-10 16:16:57 +0800

    add jwt verfication to ec2
    #22

--- ec2/index.js
+++ ec2/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 const PORT = 4000;
-const api_key = require('./.keys/api-key.js')
+const private_key = require('./.keys/private-key.js')
 
 const MongoClient = require('mongodb').MongoClient;
 const mongodbUrl = 'mongodb://localhost:27017';
@@ -46,14 +47,16 @@ const sendToMongoDatabase = (data) => {
 }
 
 app.post('/', (req, res, next) => {
-  console.log(req.body);
-  if (req.body.api_key === api_key) {
-    req.body.api_key = null;
+  try {
+    var decoded = jwt.verify(req.body.token, private_key, {algorithms:["RS256"]});
+    console.log(decoded);
     res.status(200).send("EC2 post request received, api key verified");
-    sendToMongoDatabase(req.body);
+    // sendToMongoDatabase(req.body);
     return;
+  } catch (err) {
+    console.error(err);
+    res.status(206).send("EC2 unauthorized access");
   }
-  res.status(206).send("EC2 unauthorized access");
 })
 
 app.listen(PORT, () => {
