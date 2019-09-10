38ebb8c7aa3bc4d69387d69013e643cfab169b75
seanchok <seanchok@gmail.com>
2019-09-10 23:50:04 +0800

    implement jsonwebtoken and dotenv in lambda
    #22

--- lambda/index.js
+++ lambda/index.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
-const ec2_api_key = require('./.keys/api-key');
+const jwt = require('jsonwebtoken');
+const env = require('dotenv');
+env.config();
 
 exports.handler = async (event) => {
     const data = JSON.parse(event.body);
@@ -7,10 +9,9 @@ exports.handler = async (event) => {
     const firebase_url = "https://us-central1-rcsls-2018.cloudfunctions.net/setWasherStatus"
     const firebase_response = await axios.post(firebase_url, data);
 
-    data.api_key = ec2_api_key;
-
-    const ec2_url = "http://ec2-52-15-92-86.us-east-2.compute.amazonaws.com:4000"
-    const ec2_response = await axios.post(ec2_url, data);
+    const token = await jwt.sign(data, process.env.EC2_PRIVATE_KEY, {algorithm: 'HS256'});
+    const ec2_url = "http://ec2-52-15-92-86.us-east-2.compute.amazonaws.com:4000";
+    const ec2_response = await axios.post(ec2_url, {token: token});
 
     var responseText = "Sent data to Firebase.\n" + ec2_response.data;
 
