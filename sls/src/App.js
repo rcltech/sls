6fa85e9c55bcf4b0bad60dc38c968d4b03292f13
seanchok <seanchok@gmail.com>
2020-04-14 21:52:43 +0100

    #35: fix sls website
    get washer data from phoenix with apollo
    
    fix warnings and errors

--- sls/src/App.js
+++ sls/src/App.js
@@ -1,47 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'typeface-roboto';
-import {Container} from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import withWasherStatus from './Root.js';
-import {map} from 'lodash';
+import { map } from 'lodash';
 import Washer from './components/Washer.js';
 import InfoSection from './components/InfoSection.js';
 import logo from './images/logo.png';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-const createWashers = (data) => (
-    map(data, (value, key) => (
-        <div className="item">
-          <Washer key={key} status={value}/>
-        </div>
-    ))
-);
+const createWashers = (data) =>
+  map(data, (value, key) => (
+    <div key={key} className="item">
+      <Washer status={value} />
+    </div>
+  ));
 
-function App({data}) {
+const query = gql`
+  query {
+    washers {
+      id
+      in_use
+    }
+  }
+`;
+
+function App() {
+  const [washerData, setWasherData] = useState({ 1: -1, 2: -1, 3: -1, 4: -1 });
+  const { data, error } = useQuery(query, { fetchPolicy: 'network-only' });
+
+  if (error) console.log(error);
+  useEffect(() => {
+    if (data && data.washers) {
+      const newData = {};
+      data.washers.forEach(
+        (washer) => (newData[washer.id] = washer.in_use ? 1 : 0)
+      );
+      setWasherData(newData);
+    }
+  }, [data]);
+
   return (
     <div className="App">
       <Container className="App-header">
         <div>
-          <a href='http://rctech.club' target='_blank' rel='noopener noreferrer'>
-            <img className="logo" src={logo} alt="RCTECH"/>
+          <a
+            href="http://rctech.club"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="logo" src={logo} alt="RCTECH" />
           </a>
         </div>
-        <div className="header-text">
-          Smart Laundry System
-        </div>
-
+        <div className="header-text">Smart Laundry System</div>
       </Container>
 
       <Container className="App-body">
-        <div className="container">
-          {createWashers(data)}
-        </div>
+        <div className="container">{createWashers(washerData)}</div>
 
         <InfoSection />
-
       </Container>
     </div>
   );
 }
 
-export default withWasherStatus(App);
+export default App;
--- sls/src/index.js
+++ sls/src/index.js
@@ -1,10 +1,57 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from './theme';
+import './index.css';
+import App from './App';
+import { Loading } from './components/Loading';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Index = () => {
+  const [client, setClient] = useState(undefined);
+
+  useEffect(() => {
+    const uri =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:4000/graphql'
+        : 'https://phoenix.rctech.club/graphql';
+    const link = new HttpLink({
+      uri
+    });
+
+    const cache = new InMemoryCache();
+    cache.writeData({
+      data: {}
+    });
+
+    const client = new ApolloClient({
+      cache,
+      link
+    });
+
+    setClient(client);
+  }, []);
+
+  if (!client) return <Loading />;
+
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+const IndexWithTheme = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Index />
+    </ThemeProvider>
+  );
+};
+
+ReactDOM.render(<IndexWithTheme />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
